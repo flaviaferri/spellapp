eaf6782cd56d2f08b47190f01fc8be117a3cd693
Flavia <flavia_ferri@hotmail.com>
2021-07-25 20:29:34 +0200

    Fix props

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,18 @@ import { ThemeProvider } from "@emotion/react";
 const style = css`
   @import url("https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,800;1,400&display=swap");
 
+  body {
+    min-height: 100vh;
+    min-height: -webkit-fill-available;
+  }
+
   html {
     font-family: "Nunito", sans-serif;
     font-size: 10px;
     box-sizing: border-box;
     background-color: #f9f9f9;
+    min-height: 100vh;
+    height: -webkit-fill-available;
   }
 
   * {
